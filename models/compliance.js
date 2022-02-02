a5ad19e412c70b1a64c8114620a34de099648673
Andrew Chen <shiki510520.com>
2022-02-03 08:59:17 +0900

    add models - compliances

---
+++ models/compliance.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+
+const ComplianceSchema = new mongoose.Schema({
+    schema_version: {
+        type: Number,
+        value: 1,
+        required: true
+    },
+    report_number: {
+        type: String,
+        required: true
+    },
+    record_type: {
+        type: {
+            type: String 
+        },
+        record_detail: {
+            type: String
+        }
+    },
+    issued_date: {
+        type: Date,
+        required: true
+    },
+    expiry_date: {
+        type: Date,
+        required: true
+    }
+})
+
+mongoose.model('Compliance', ComplianceSchema)
--- server.js
+++ server.js
@@ -3,7 +3,6 @@ require("dotenv").config({path: "./config.env"})
 //npm packages
 const express = require('express')
 const app = express()
-
 const PORT = process.env.PORT
 
 //connect to MongoDB by mongoose
@@ -15,10 +14,17 @@ const db = mongoose.connection
 db.on('error', err => console.error(err))
 db.once('open', () => console.log('connected to Mongoose'))
 
-//
+
+//Data parasing
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+
 app.get('/', (req, res) => {
     res.send("Inside Home")
+
 })
+
 
 //set the listen port and show the successful connect information
 app.listen(process.env.PORT, console.log(`Server is starting at ${PORT}`));
