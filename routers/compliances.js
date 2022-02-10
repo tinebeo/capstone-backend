4668de7633ca799057fe375f57479c70639cbdce
Andrew Chen <shiki510520.com>
2022-02-10 12:43:02 +0900

    unfinish the search operator

--- routers/compliances.js
+++ routers/compliances.js
@@ -2,12 +2,12 @@ const express = require('express')
 const Compliance = require('../models/compliance')
 const router = express.Router()
 
-//post data from MongoDB
+// get to the front-end page
 router.get('/', async (req, res) => {
     res.render('compliances/insert', {compliance: new Compliance() })
 })
 
-// create the new complinace
+// create the new complinace to MongoDB
 router.post('/', async (req, res) => {
     const compliance = new Compliance({
         report_number: req.body.reportNumber,
@@ -19,24 +19,33 @@ router.post('/', async (req, res) => {
         expiry_date: req.body.expiryDate
     })
     compliance.save()
-    res.send(compliance)
+    res.redirect('/compliances')
 })
 
 
-
-//save data into MongoDB
-//  const compliance = new Compliance({
-//     schema_version: 1,
-//     report_number: 'N0142356',
-//     record_type: {
-//         type: 'Reporitng test',
-//         record_detail: 'this is only for testing purpose'
-//     },
-//     issued_date: '2022/02/01',
-//     expiry_date: '2022/02/02'
-// })
-// compliance.save()
-// console.log(compliance)
+//find data into MongoDB
+router.get('/search', async (req, res) => {
+    let query = Compliance.find()
+    if (req.query.reportNumber != null && req.query.reportNumber != ''){
+        query = req.query.reportNumber
+    }
+    // if (req.query.issued_date != null && req.query.issued_date != ''){
+    //     query = query.lte('issued_date', req.query.issued_date)
+    // }
+    // if (req.query.expiry_date != null && req.query.expiry_date!= ''){
+    //     query = query.gte('expiry_date', req.query.expiry_date)
+    // }
+    try {
+        const compliances = await query.exec()
+        res.render('compliances/search', {
+            compliances: compliances,
+            searchOptions: req.query
+        })
+        res.redirect('compliances/search')
+    } catch {
+        res.redirect('/')
+    }
+})
 
 
 module.exports = router
