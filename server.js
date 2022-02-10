fb9dc00f100c1c99b94c67092e11482e78752366
caebeo <caebeo@hotmail.com>
2022-02-10 13:56:53 -0500

    CE - adding access control

--- server.js
+++ server.js
@@ -32,6 +32,15 @@ const db = mongoose.connection
 db.on('error', err => console.error(err))
 db.once('open', () => console.log('connected to Mongoose'))
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+  });
+  
+
 //connect router
 app.use('/', indexRouter)
 app.use('/users', userRouter)
