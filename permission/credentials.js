c97adea0ac7a6f7ebfafcb05fd8053a3c29a0d43
Andrew Chen <shiki510520.com>
2022-03-10 02:55:44 +0900

    fix the credentials bugs

--- permission/credentials.js
+++ permission/credentials.js
@@ -3,7 +3,10 @@ const allowedOrigins = require('../config/allowedOrigins')
 const credentials = (req, res, next) => {
     const origin = req.headers.origin
     if (allowedOrigins.includes(origin)){
-        res.headers('Access-Control-Allow-Credentials', true)
+        res.header("Access-Control-Allow-Origin", '*')
+        res.header('Access-Control-Allow-Credentials', true)
+        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+        res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json')
     }
     next()
 }
