c4b9f3577019fe636183d59540321bca0045516f
Andrew Chen <shiki510520.com>
2022-03-18 10:11:52 +0900

    add new allowed origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
   'http://localhost:3000',
-  'https://capstone-humber.netlify.app'
+  'https://capstone-humber.netlify.app',
+  'https://humber-capstone-backend.herokuapp.com'
 ]
 
 module.exports = allowedOrigins
