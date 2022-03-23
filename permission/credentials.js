ea00f3843640d90d32da125fe40b86a7911f8bf5
Andrew Chen <shiki510520.com>
2022-03-23 12:00:28 +0900

    edit the access-control-allow-headers

--- permission/credentials.js
+++ permission/credentials.js
@@ -6,9 +6,8 @@ const credentials = (req, res, next) => {
         res.header('Access-Control-Allow-Origin', '*')
         res.header('Access-Control-Allow-Credentials', true)
         res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-        //res.header('Access-Control-Allow-Headers', 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json')
-        res.header('Access-Control-Allow-Headers', 
-            'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers')
+        res.header('Access-Control-Allow-Headers', 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json')
+        //res.header('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers')
     }
     next()
 }
