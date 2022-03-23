6dcaadbee4cd00b93e0794014986350558eb7044
Andrew Chen <shiki510520.com>
2022-03-24 01:05:36 +0900

    edit the access-control-allow methods

--- permission/credentials.js
+++ permission/credentials.js
@@ -5,7 +5,7 @@ const credentials = (req, res, next) => {
     if (allowedOrigins.includes(origin)){
         res.header('Access-Control-Allow-Origin', '*');
         res.header('Access-Control-Allow-Credentials', true);
-        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+        res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
         res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     }
     next()
