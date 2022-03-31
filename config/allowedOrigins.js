3b785b52c528a73a3c385647980e28f3d2852df9
Andrew Chen <shiki510520.com>
2022-04-01 04:13:23 +0900

    cors headaches

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,6 @@
 const allowedOrigins = [
   'http://localhost:3000',
-  'https://capstone-humber.netlify.app',
-  'https://js.stripe.com/'
+  'https://capstone-humber.netlify.app'
 ]
 
 module.exports = allowedOrigins
--- permission/credentials.js
+++ permission/credentials.js
@@ -6,7 +6,7 @@ const credentials = (req, res, next) => {
         res.header('Access-Control-Allow-Origin', '*');
         res.header('Access-Control-Allow-Credentials', true);
         res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
-        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     }
     next()
 }
