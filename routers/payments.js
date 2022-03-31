51b5f25516610e93f7cb899d89f11b5fd193854a
Andrew Chen <shiki510520.com>
2022-04-01 02:01:53 +0900

    payment with Stripe

--- routers/payments.js
+++ routers/payments.js
@@ -2,21 +2,27 @@ require("dotenv").config({ path: "../config/config.env" })
 const express = require('express')
 const router = express.Router()
 //const stripePublicKey = process.env.STRIPE_PUBLIC_KEY
-//const stripeSecretKey = process.env.STRIPE_SECRET_KEY
-const Stripe = require('stripe')
+const stripeSecretKey = process.env.stripe_secretKey
+const Stripe = require('stripe')(stripeSecretKey)
 
 // get bundles from subscriber 
-router.post('/payment', (req, res) => {
+router.post('/payment', async (req, res) => {
     const price = req.body.price
     const stripeId = req.body.id
+    try {
+        const payment = await Stripe.paymentIntents.create({
+            amount: price,
+            currency: 'USD',
+            payment_method: stripeId,
+            confirm: true
+        })
+        console.log(payment)
 
-    Stripe.charges.create({
-        amount: price,
-        source: stripeId,
-        currency: 'cad'
-    }).then(() => {
-        return res.status(201).send({message: "Successfully charged the fees."})
-    }).catch((err) => {
+        return res.status(200).send({message: "Successfully charged the fees."})
+
+    } catch (error) {
+        
         return res.status(500).send({message: "charge fail!"}, err)
-    })
+    }
+
 })
