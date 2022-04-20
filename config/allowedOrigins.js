3f6c4991e3fdf1ab8d077d121688533d5bc063fa
Andrew Chen <shiki510520.com>
2022-04-20 23:19:26 +0900

    add URL for CORS

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
   'http://localhost:3000',
-  'https://capstone-humber.netlify.app'
+  'https://capstone-humber.netlify.app',
+  'https://bug-mui-migration--capstone-humber.netlify.app'
 ]
 
 module.exports = allowedOrigins
