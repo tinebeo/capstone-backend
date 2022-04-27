1178ac2d746874613820e0437c8154672dca7693
caebeo <caebeo@hotmail.com>
2022-04-27 12:35:18 -0400

    added new domain

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,9 @@
 const allowedOrigins = [
   'http://localhost:3000',
   'https://capstone-humber.netlify.app',
-  'https://bug-mui-migration--capstone-humber.netlify.app'
+  'https://bug-mui-migration--capstone-humber.netlify.app',
+  'https://dailycompliance.netlify.app'
+
 ]
 
 module.exports = allowedOrigins
